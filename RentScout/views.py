1401f09d54bae1c52a8cd68df21d7ee6469e57ea
felamin2023 <kkangpae59@gmail.com>
2024-09-17 20:08:55 +0800

    signin page: layout

--- RentScout/views.py
+++ RentScout/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
+@login_required
 def home(request):
     return render(request, 'RentScout/home.html', {})
+
+def signinpage(request):
+    return render(request, 'RentScout/signin.html', {})
--- appdev/settings.py
+++ appdev/settings.py
@@ -136,3 +136,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'signin'
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
