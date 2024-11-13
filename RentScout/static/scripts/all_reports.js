8257f864a6410db421c85d8b325a3b6cf38de2f1
felamin2023 <kkangpae59@gmail.com>
2024-11-13 22:49:47 +0800

    report page: design done

--- RentScout/static/scripts/all_reports.js
+++ RentScout/static/scripts/all_reports.js
@@ -51,7 +51,7 @@ $(document).ready(function(){
     })
 
     function get_all_reports(){
-        reports_container.empty();
+        $('#reports_container').empty();
         $.ajax({
             url: '/buildings/report/get_all/',
             type: 'GET',
@@ -59,77 +59,88 @@ $(document).ready(function(){
                 console.log(response);
                 $.each(response.reports, function(index, report){
                     console.log(response.reports);
-
+    
                     let report_item = $('<div></div>', {
                         class: 'reports'
-                    })
-
+                    });
+    
                     let left_container = $('<div></div>', {
                         class: 'left_container'
-                    })
-
+                    });
+    
                     let right_container = $('<div></div>', {
                         class: 'right_container'
-                    })
+                    });
                     
                     let buttons_container = $('<div></div>', {
                         class: 'buttons_container'
-                    })
-
+                    });
+    
                     let image = $('<img>', {
                         src: report.building_image,
                         alt: `Image of ${report.building_name}`,
                         class: 'report_images'
-                    })
-
+                    });
+    
                     let header = $('<h3></h3>', {
                         class: 'report_header',
                         text: `${report.building_name} by ${report.reporter}`
-                    })
-
+                    });
+    
                     let reason = $('<p></p>', {
                         text: report.reason
-                    })
-
+                    });
+    
+                    let reasontxt = $('<h5></h5>', {
+                        text: "REASON"
+                    });
+    
                     let review = $('<button></button>', {
                         class: 'review_btn',
                         value: report.buildingid,
                         text: 'Review'
-                    })
-
+                    });
+    
                     let ban_building = $('<button></button>', {
                         class: 'ban_btn',
                         value: report.buildingid,
                         text: 'Ban Building'
-                    })
-
+                    });
+    
                     let deny = $('<button></button>', {
                         class: 'deny_btn',
                         value: report.reportid,
                         text: 'Deny Report'
-                    })
-
-
+                    });
+    
+                    // Set up click event for image
+                    image.on('click', function() {
+                        $('#photo_view_img').attr('src', report.building_image);
+                        $('#photo_view_img').attr('alt', `Image of ${report.building_name}`);
+                        $('.selected_photo').removeClass('hidden'); 
+                    });
+    
                     left_container.append(image);
-
+    
                     right_container.append(header);
+                    right_container.append(reasontxt);
                     right_container.append(reason);
-
+    
                     buttons_container.append(review);
                     buttons_container.append(ban_building);
                     buttons_container.append(deny);
-
-                    report_item.append(left_container);
+    
                     report_item.append(right_container);
                     report_item.append(buttons_container);
-
-                    reports_container.append(report_item);
-                })
+                    report_item.append(left_container);
+                    $('#reports_container').append(report_item);
+                });
             }, 
             error: function(xhr, status, error){
                 console.log(error);
                 alert(`Error: ${xhr.responseText.error || error}`);
             }
-        })
-    }   
+        });
+    }
+     
 });
